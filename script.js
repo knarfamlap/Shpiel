6d0aeb5dc92356cd7d9210e556b7f591f9dae6bc
Frank Palma <frankpalma12@gmail.com>
2018-08-12 13:45:07 -0400

    random pic on login

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// Login Page slideShow
+let pics = ["https://78.media.tumblr.com/890eedd4b23b25347264f6ac8c8e6b98/tumblr_p33fo10WTj1r6pbleo2_1280.png",
+"https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg",
+"https://get.pxhere.com/photo/street-wall-blue-colorful-graffiti-street-art-art-mural-greece-athens-work-of-art-painting-wall-1195942.jpg",
+"https://upload.wikimedia.org/wikipedia/commons/9/90/Touched_by_His_Noodly_Appendage_HD.jpg"];
 
-let cycle = 0;
+let num = Math.floor(Math.random() * (pics.length - 1));
 
-let backgroundPics = ["https://78.media.tumblr.com/890eedd4b23b25347264f6ac8c8e6b98/tumblr_p33fo10WTj1r6pbleo2_1280.png", "https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg", "https://get.pxhere.com/photo/street-wall-blue-colorful-graffiti-street-art-art-mural-greece-athens-work-of-art-painting-wall-1195942.jpg", "https://upload.wikimedia.org/wikipedia/commons/9/90/Touched_by_His_Noodly_Appendage_HD.jpg"];
+let bg = document.getElementById('bg');
 
-setInterval(function() {
-  if (cycle < 4) {
-    document.body.style.backgroundImage = "url('" + backgroundPics[cycle] + "')";
-    cycle += 1;
-  } else {
-    cycle = 0;
-  }
-}, 5000);
+bg.style.backgroundImage = 'url(' + pics[num] + ')';
