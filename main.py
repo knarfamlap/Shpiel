5d600a07a4955c23f689bb4532c2e9a53e466211
Frank Palma <frankpalma12@gmail.com>
2018-08-13 10:22:17 -0400

    Created html templates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webapp2;
 import jinja2;
+import os
 
 jina_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -12,6 +13,55 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('Hello World')
 
 
+class News(webapp2.RequestHandler):
+    def get(self):
+
+        
+
+class Sports(webapp2.RequestHandler):
+    def get(self):
+
+
+
+class Tech(webapp2.RequestHandler):
+    def get(self):
+
+
+
+class FeelGood(webapp2.RequestHandler):
+    def get(self):
+
+
+
+class Business(webapp2.RequestHandler):
+    def get(self):
+
+
+
+class Caleb(webapp2.RequestHandler):
+    def get(self):
+
+
+
+class Books(webapp2.RequestHandler):
+    def get(self):
+
+
+
+class Education(webapp2.RequestHandler):
+    def get(self):
+
+
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/news', News),
+    ('/sports', Sports),
+    ('/tech', Tech),
+    ('/feelgood', FeelGood),
+    ('/business' Business),
+    ('/celeb', Caleb),
+    ('/books', Books),
+    ('/education', Education)
 ], debug=True)
