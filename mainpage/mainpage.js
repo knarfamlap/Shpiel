9a2004918c031462a24ff2eaec9d4ac7d8fea72c
Madelyn Greene <madelg25@gmail.com>
2018-08-14 10:07:29 -0400

    Edited sidebar info

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -1,5 +1,11 @@
-let buttons = querySelector('#loginsignupbutton');
-
-buttons.addEventListener('click', () => {
-
-})
+// let box1 = document.querySelector('#box1');
+//
+// box1.addEventListener('click', () => {
+//   login.innerHTML =
+// });
+//
+// let box2 = document.querySelector('#box2');
+//
+// signup.addEventListener('click', () => {
+//   signup.innerHTML =
+// });
